16e2d6d0fdcd1e9e05b13427f5cc7b3686dd5fb2
FourSeasonK <fourseasonk77@gmail.com>
2023-02-25 01:23:18 -0800

    edit features and adding splash and image asset

--- HiD/app/src/main/java/com/example/hid/activities/NavigationActivity.java
+++ HiD/app/src/main/java/com/example/hid/activities/NavigationActivity.java
@@ -3,22 +3,29 @@ package com.example.hid.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hid.R;
 import com.example.hid.created.CreateMyDActivity;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class NavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -28,9 +35,12 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
     private DrawerLayout drawer;
     ActionBarDrawerToggle toggle;
     View logInOut;
+    TextView userEmail;
+//    ArrayList<String> userInfo;
 
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authListener;
+    boolean checkUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,31 +54,20 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
 //            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 //                FirebaseUser user = firebaseAuth.getCurrentUser();
 //
-//
-//                if(user == null){
-//                    initView();
-//                    frameLayout = (FrameLayout) findViewById(R.id.container);
-//                    drawer = findViewById(R.id.drawer_layout);
-//                    logInOut = findViewById(R.id.nav_login);
-//                    NavigationView navigationView = findViewById(R.id.nav_view);
-//
-//                    navigationView.setNavigationItemSelectedListener(NavigationActivity.this);
+//                if (user != null) {
+//                    checkUser = true;
 //                } else {
-//                    loginView();
-//                    frameLayout = (FrameLayout) findViewById(R.id.containerlogin);
-//                    drawer = findViewById(R.id.drawer_layout_login);
-//                    logInOut = findViewById(R.id.nav_login);
-//                    NavigationView navigationView = findViewById(R.id.nav_viewlogin);
-//
-//                    navigationView.setNavigationItemSelectedListener(NavigationActivity.this);
+//                    checkUser = false;
 //                }
 //            }
 //        };
 
         initView();
+//        checkCurrentUser(userInfo);
+//        loginView();
         frameLayout = (FrameLayout) findViewById(R.id.container);
         drawer = findViewById(R.id.drawer_layout);
-        logInOut = findViewById(R.id.nav_login);
+//        logInOut = findViewById(R.id.nav_login);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
         navigationView.setNavigationItemSelectedListener(this);
@@ -105,16 +104,16 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private void loginView(){
-        toolbar = findViewById(R.id.toolbarlogin);
-        drawer = findViewById(R.id.drawer_layout_login);
-        NavigationView navigationView = findViewById(R.id.nav_viewlogin);
-
-        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_nav, R.string.close_nav);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
-    }
+//    private void loginView(){
+//        toolbar = findViewById(R.id.toolbarlogin);
+//        drawer = findViewById(R.id.drawer_layout_login);
+//        NavigationView navigationView = findViewById(R.id.nav_viewlogin);
+//
+//        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_nav, R.string.close_nav);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//        navigationView.setNavigationItemSelectedListener(this);
+//    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -122,6 +121,19 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
 //        SharedPreferences sh = getSharedPreferences("LogInSuccess", 0);
 //        boolean logIn = sh.getBoolean("LOGINVALUE", false);
 //        Log.d("LogInBoolValue", String.valueOf(logIn));
+
+
+//            ArrayList<String> userInfo = new ArrayList<>();
+//            checkCurrentUser(userInfo);
+//
+//            userEmail = findViewById(R.id.txtUserName);
+//
+//            userEmail.setText(userInfo.get(0));
+//
+//            Log.d(this.getClass().getSimpleName(), userEmail.getText().toString());
+//            Toast.makeText(this, "Welcome! " + userEmail.getText().toString(),
+//                    Toast.LENGTH_LONG).show();
+
 
         menuItem.setChecked(true);
 
@@ -165,81 +177,56 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
                     break;
 
                 case R.id.nav_login:
+
+                    if(checkUser == true){
+                        menuItem.setIcon(R.drawable.nav_logout);
+                        menuItem.setTitle("Log out");
+                    }
+
                     Intent switchToLogInIntent = new Intent(this, LogInOutActivity.class);
                     startActivity(switchToLogInIntent);
                     break;
-
-                case R.id.nav_logout:
-                    auth.signOut();
-                    Intent switchToLogOutIntent = new Intent(this, LogInOutActivity.class);
-                    startActivity(switchToLogOutIntent);
-                    break;
-
-                case R.id.nav_userInfo:
-                    Intent switchToUserInfoIntent = new Intent(this, UserDashboardActivity.class);
-                    startActivity(switchToUserInfoIntent);
-
-                case R.id.nav_password:
-                    Intent switchToPasswordInIntent = new Intent(this, ResetPasswordActivity.class);
-                    startActivity(switchToPasswordInIntent);
-                    break;
+//
+//                case R.id.nav_userInfo:
+//                    Intent switchToUserInfoIntent = new Intent(this, UserDashboardActivity.class);
+//                    startActivity(switchToUserInfoIntent);
+//
+//                case R.id.nav_password:
+//                    Intent switchToPasswordInIntent = new Intent(this, ResetPasswordActivity.class);
+//                    startActivity(switchToPasswordInIntent);
+//                    break;
             }
-//        } else {
-//            switch (menuItem.getItemId()) {
-//                case R.id.nav_home:
-//                    Intent switchToHomeIntent = new Intent(this, HomeActivity.class);
-//                    startActivity(switchToHomeIntent);
-//                    break;
-//
-//                case R.id.nav_boxBreath:
-//                    Intent switchToBoxBIntent = new Intent(this, BoxBreathingActivity.class);
-//                    startActivity(switchToBoxBIntent);
-//                    break;
-//
-//                case R.id.nav_donotB:
-//                    Intent switchToDonotBIntent = new Intent(this, DoNotBlameActivity.class);
-//                    startActivity(switchToDonotBIntent);
-//                    break;
-//
-//                case R.id.nav_createD:
-//                    Intent switchToCreateDIntent = new Intent(this, CreateMyDActivity.class);
-//                    startActivity(switchToCreateDIntent);
-//                    break;
-//
-//                case R.id.nav_DForum:
-//                    Intent switchToDForumIntent = new Intent(this, ShareForumActivity.class);
-//                    startActivity(switchToDForumIntent);
-//                    break;
-//
-//                case R.id.nav_contactP:
-//                    Intent switchToContactPIntent = new Intent(this, ContactPeopleActivity.class);
-//                    startActivity(switchToContactPIntent);
-//                    break;
-//
-//                case R.id.nav_userDashboaed:
-//                    Intent switchToUserDashBIntent = new Intent(this, UserDashboardActivity.class);
-//                    startActivity(switchToUserDashBIntent);
-//                    break;
-//
-//                case R.id.nav_login:
-//                    Intent switchToLogInIntent = new Intent(this, LogInOutActivity.class);
-//                    startActivity(switchToLogInIntent);
-//                    break;
-//
-//            }
-//        }
 
             return true;
         }
 
 
-    @Override
-    public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+//    @Override
+//    public void onBackPressed() {
+//        if(drawer.isDrawerOpen(GravityCompat.START)){
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
 
+    public void checkCurrentUser(ArrayList<String> userInfo) {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        userInfo = new ArrayList<>();
+
+        if (user != null) {
+            // User is signed in
+            String email = user.getEmail();
+            String uid = user.getUid();
+            //String name = user.getDisplayName();
+            //emailAdd = email;
+            userInfo.add(email);
+            userInfo.add(uid);
+            checkUser = true;
+
+        } else {
+            checkUser = false;
+        }
     }
 }
--- HiD/app/src/main/java/com/example/hid/activities/SplashActivity.java
+++ HiD/app/src/main/java/com/example/hid/activities/SplashActivity.java
@@ -2,9 +2,13 @@ package com.example.hid.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.hid.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +16,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LogInOutActivity.class));
+
+            }
+        };
+
+        Timer opening = new Timer();
+        opening.schedule(task, 3000);
     }
 }
